2aba870dd1a40abc340a895e52bb0cfce05dcd53
phil-shenk <shenk.philip@gmail.com>
2024-01-27 14:36:36 -0500

    baltimore

--- Baltmore.gd
+++ Baltmore.gd
@@ -1,5 +1,6 @@
 extends Level
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
