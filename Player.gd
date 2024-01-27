165fa047f986d57395b21de6dc23bcecb0d2246a
welbeebee <welby.dev@gmail.com>
2024-01-27 10:18:32 -0500

    Update Player.gd
    start new movement on player.gd

--- Player.gd
+++ Player.gd
@@ -7,6 +7,15 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var input_dir = Vector3.ZERO
+var direction = Vector3.ZERO
+var move_rot: float = 0
+
+#TO-DO: add controllable camera script
+@onready var camera
+
+var horizontal_velocity: Vector3 = Vector3.ZERO
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,8 +28,8 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -29,3 +38,25 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+
+
+func set_horizontal_movement(speed, turn_speed, cam_follow_speed, acceleration, delta):
+	# get the movement direction directly from the controls node and turn it into a Vector3
+	var move_dir = direction
+	var direction = Vector3(move_dir.x, 0, move_dir.y)
+
+	# make the player move towards where the camera is facing by lerping the current movement rotation
+	# towards the camera's horizontal rotation and rotating the raw movement direction with that angle
+	move_rot = lerpf(move_rot, deg_to_rad(camera._rot_h), cam_follow_speed * delta)
+	direction = direction.rotated(Vector3.UP, move_rot)
+
+	# lerp the player's current horizontal velocity towards the horizontal velocity as determined by
+	# the input direction and the given horizontal speed
+	horizontal_velocity = lerp(horizontal_velocity, direction * speed, acceleration * delta)
+
+	# if the player has any amount of movement, lerp the player model's rotation towards the current
+	# movement direction based checked its angle towards the X+ axis checked the XZ plane
+	if move_dir != Vector2.ZERO:
+		#TO-DO: add mesh model rotation here
+		#skin.rotation.y = lerp_angle(skin.rotation.y, atan2(-direction.x, -direction.z), turn_speed * delta)
